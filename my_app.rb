0dfb1a69d360ef025db5fe398e2b66ca49638e4b
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-06-17 15:02:08 +0100

    Added feature to add bookmarks and refactored

--- my_app.rb
+++ my_app.rb
@@ -16,6 +16,15 @@ class MyApp < Sinatra::Base
     erb :index
   end
 
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url])
+    redirect '/bookmarks'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
